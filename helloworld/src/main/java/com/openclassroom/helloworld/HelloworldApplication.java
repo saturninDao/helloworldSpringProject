0061523072eb3b97e55c70f7cad6c19f5669ffc6
Saturnin Dao <saturnindao@gmail.com>
2021-08-31 15:50:55 +0000

    Hello World

--- helloworld/src/main/java/com/openclassroom/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/openclassroom/helloworld/HelloworldApplication.java
@@ -1,13 +1,18 @@
 package com.openclassroom.helloworld;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloworldApplication {
+public class HelloworldApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World my Boss Saturnin");
+	}
 }
