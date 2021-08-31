93c66b46b6bda26d54a706d6f876cb1b2793932f
Saturnin Dao <saturnindao@gmail.com>
2021-08-31 16:11:25 +0000

    Hello World with model and service

--- helloworld/src/main/java/com/openclassroom/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/openclassroom/helloworld/HelloworldApplication.java
@@ -1,5 +1,8 @@
 package com.openclassroom.helloworld;
 
+import com.openclassroom.helloworld.model.HelloWorld;
+import com.openclassroom.helloworld.service.BusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloworldApplication implements CommandLineRunner {
 
+	@Autowired
+	private BusinessService bs;
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World my Boss Saturnin");
+		//System.out.println("Hello World my Boss Saturnin");
+		HelloWorld hw = bs.getHelloWorld();
+		System.out.println(hw);
 	}
 }
